3d0c3f626ea651350040598040887535ef538cb6
Filip Koźlicki <filip.kozlickii@gmail.com>
2022-06-19 20:24:44 +0200

    Components exports file

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './Header/Header';
+export { default as InputBar } from './InputBar/InputBar';
+export { default as TodoList } from './TodoList/TodoList';
+export { default as FilterBar } from './FilterBar/FilterBar';
